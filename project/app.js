91388e2f8e7ffc3be92669a8b8cccb509eec0f4f
kamal kishore <kamal.kishore981@gmail.com>
2022-08-04 16:25:39 -0400

    added questions

--- project/app.js
+++ project/app.js
@@ -1,16 +1,97 @@
 // This needs to be added in order to use file system
 const inquirer = require('inquirer');
-inquirer.prompt([
+
+const promptUser = () =>{
+return inquirer.prompt([
   {
 
     type:'input',
     name :'name',
     message:'What is your name?'
 
+  },
+  { 
+    type:'input',
+    name:'github',
+    message:'Enter your Github Username'
+  },
+  {
+    type:'input',
+    name:'about',
+    message:'provide some information about yourself:'
+
   }
 
-])
-.then(answer => console.log(answer));
+
+]);
+};
+
+// Add function promptProject() for the project question
+const promptProject = portfolioData =>{
+   portfolioData.projects = [];
+  //  if there is no 'project' array property, create on
+if (!portfolioData.projects){
+  portfolioData.projects=[];
+}
+  console.log(`
+  ================
+  Add a New project
+  `);
+  return inquirer.prompt([
+    {
+      type:'input',
+      name:'name',
+      message:'What is the name of your project?'
+    },
+    {
+      type:'input',
+      name:'description',
+      message:'Provide a description of the project(Require)'
+    },
+    {
+      type:'checkbox',
+      name:'languages',
+      message:'What did you build this project with?(Check all that apply )',
+      choices:['javaScript','HTML','CSS','ES6','jQuery','Bootstrap','Node']
+    },
+    {
+      type:'input',
+      name:'link',
+      message:'Enter the Github link to your project.(Required)'
+    },
+    {
+      type:'confirm',
+      name:'feature',
+      message:'Would you like to feature this project?',
+      default:false
+    },
+    {
+      type:'confirm',
+      name:'confirmAddProject',
+      message:'would you like to enter another project?',
+      default:false
+    }
+  ]).then(projectData =>{
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject){
+      // if the user like to add another project promptProject ()function will be call
+      return promptProject(portfolioData);
+    }else{
+      return portfolioData;
+    }
+
+  });
+   
+};
+
+
+
+promptUser()
+
+.then(promptProject)
+.then(portfolioData =>{
+  console.log(portfolioData);
+});
 
 
 
